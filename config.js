b80850569e62eb15ccfa21da4e5f723fbaa1fc8c
AdamRepaskySykoraIT <adam.repasky@sykorait.com>
2025-07-24 16:19:19 +0200

    Update importId

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    IMPORT_SOURCE_ID: '2addeb7d-c156-4062-8783-030b9fcd3016', // from the Forge import config
+    IMPORT_SOURCE_ID: '97cf971c-b6fa-495d-a2ab-03f25875d84a', // from the Forge import config
     WORKSPACE_ID: '88438e77-439f-4ab6-88f6-eee43d8cb2a6',
     JIRA_USER: 'dev.automation.user@proton.me',
     JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
