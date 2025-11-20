46949e9bb6b2288a2d6ed321074890d2be811594
xfischerMartin <102572401+xfischerMartin@users.noreply.github.com>
2025-11-20 11:02:08 +0100

    Update config.js
    new testing environment

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 module.exports = {
-    IMPORT_SOURCE_ID: '97cf971c-b6fa-495d-a2ab-03f25875d84a', // from the Forge import config
-    WORKSPACE_ID: '88438e77-439f-4ab6-88f6-eee43d8cb2a6',
-    JIRA_USER: 'dev.automation.user@proton.me',
-    JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
-    WEBTRIGGER_URL: process.env.WEBTRIGGER_URL
+    // ADAMS ENV
+    // IMPORT_SOURCE_ID: '97cf971c-b6fa-495d-a2ab-03f25875d84a', // from the Forge import config
+    // WORKSPACE_ID: '88438e77-439f-4ab6-88f6-eee43d8cb2a6',
+    // JIRA_USER: 'dev.automation.user@proton.me',
+    // JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
+    // WEBTRIGGER_URL: process.env.WEBTRIGGER_URL
+
+    //mfischer.atlassian.net
+    IMPORT_SOURCE_ID: '965ce737-aef7-4a8c-a8d8-ab9b2e679e92', // from the Forge import config
+    WORKSPACE_ID: '15ea98cf-ce05-4384-b983-53c44a1a1710',
+    JIRA_USER: 'martin.fischer@sykorait.com',
+    JIRA_API_TOKEN: 'oISh7AZKSJtU9wgcxyVK9DDF',
+    WEBTRIGGER_URL: 'https://5c433676-525e-4481-9601-5a4227da8cea.hello.atlassian-dev.net/x1/oYP6Vpj0uL27syLjwsVS-GZBC9E'
 }
